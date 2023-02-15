5c46bc92a5da7b46115290ceb1672ba8d2f67941
ThearySAMRIT <somrithsotheary@gmail.com>
2023-02-15 18:10:46 +0700

    Update footer

--- src/components/cards/AuthFooter.js
+++ src/components/cards/AuthFooter.js
@@ -16,42 +16,13 @@ const AuthFooter = () => {
       >
         <Typography variant="subtitle2" color="secondary" component="span">
           This site is protected by{' '}
-          <Typography component={Link} variant="subtitle2" href="#mantis-privacy" target="_blank" underline="hover">
-            Privacy Policy
+            <Typography component={Link} variant="subtitle2" href="https://www.nbc.gov.kh/" target="_blank" underline="hover">
+            National Bank of Cambodia
           </Typography>
-        </Typography>
+        </Typography> 
 
         <Stack direction={matchDownSM ? 'column' : 'row'} spacing={matchDownSM ? 1 : 3} textAlign={matchDownSM ? 'center' : 'inherit'}>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://codedthemes.com"
-            target="_blank"
-            underline="hover"
-          >
-            Terms and Conditions
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://codedthemes.com"
-            target="_blank"
-            underline="hover"
-          >
-            Privacy Policy
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://codedthemes.com"
-            target="_blank"
-            underline="hover"
-          >
-            CA Privacy Notice
-          </Typography>
+           <Typography variant="subtitle2" color="secondary" underline="hover">2023 &copy; All rights reserved </Typography>
         </Stack>
       </Stack>
     </Container>
